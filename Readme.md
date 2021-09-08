c47bb314eeb50810a2ac99329c1a1697e3e5f934
Adriel Lucas <adriellucas1049@gmail.com>
2021-09-08 20:09:59 -0300

    read criado

---
+++ Readme.md
@@ -0,0 +1,12 @@
+ # Projeto Responsive Side Bar
+
+ # Introdução
+
+ Nesse projeto, foi desenvolvido um site responsivo unsando flexblox...
+  
+  * Esse projeto foi baseado num tutorial no site [CodingLab](https://www.codinglabweb.com/2021/04/responsive-side-navigation-bar-in-html.html).
+  * Quais tecnologias foram uutilizadas?
+    (HTML, CSS, JavaScript).
+  * O que foi aprendido/exercitado durante o projeto?(erros e acerto cometidos)
+  * Você fez ateração do projeto original?Qual foi?(corrigir algum bug, adicionou coisa, etc)
+  * Colocar um link para site hospedado
---
+++ js/functionsbar.js
@@ -0,0 +1,22 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //Chamando a função
+});
+
+searchBtn.addEventListener("click", () => { // Barra lateral aberta quando você clica no ícone de pesquisa
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //chamando a função 
+});
+
+// código para alterar o botão da barra lateral
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //substituindo a classe de ícones
+    }
+}
